61702485156eb670c5a423e227d7ba0ccd682167
Surajit-FR <miltonbaker.psoriatic@gmail.com>
2024-06-11 20:25:18 +0530

    update

--- model/validator/cart.validate.js
+++ model/validator/cart.validate.js
@@ -4,7 +4,6 @@ module.exports = (CartModel) => {
     const CartSchema = JOI.object({
         product: JOI.string().required().messages({
             "string.empty": "Product ID is required!",
-            "any.required": "Product ID is required!"
         }),
         cart_quantity: JOI.number().integer().required().messages({
             "number.base": "Quantity must be a number!",
